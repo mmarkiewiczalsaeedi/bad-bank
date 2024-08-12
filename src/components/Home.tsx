e973e83b46da9a424fcf440671a8f6c3c67ca76e
Michał Markiewicz-Al Saeedi <m.markiewiczalsaeedi@gmail.com>
2024-08-12 19:58:49 +0200

    Update

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -14,7 +14,7 @@ export const Home = () => {
 				margin: "2em",
 			}}
 		>
-			<h1>Welcome to Bad Bank{user?.email ? `, ${user.email}` : ""}!!</h1>
+			<h1>Welcome to Blissful Bank {user?.email ? `, ${user.email}` : ""}!!</h1>
 			<h2>Now with added security and authentication!*</h2>
 			<p>*everyone can see your email and password on AllData page.</p>
 		</Stack>
